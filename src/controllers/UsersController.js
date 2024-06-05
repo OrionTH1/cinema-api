737c036a999714fd63c4cb4bcc037a62bb135f4c
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2024-06-05 16:08:30 -0300

    feat: add create user in UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,22 @@
-// const knex = require("../../knexfile");
+const connection = require("../database/knex");
+const ApiError = require("../utils/ApiError");
 
 class UsersController {
   async create(request, reponse) {
     const { name, email, password } = request.body;
 
-    // await knex("users").insert(name);
+    // Checks if all the data is sent
+    if (!name || !email || !password) {
+      throw new ApiError("Name, email and password is required!", 400);
+    }
 
-    reponse.status(201).json();
+    // Create user in Table Users
+    await connection("users").insert({ name, email, password });
+
+    reponse.status(201).json({
+      status: "success",
+      message: "User created successfully",
+    });
   }
 }
 

83176f4fa8574f8106db73577b9fad4ae5e2a927
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2024-06-05 16:07:39 -0300

    feat: add Error pattern

--- src/server.js
+++ src/server.js
@@ -15,6 +15,21 @@ app.use(express.json());
 // Import Routes
 app.use(routes);
 
+app.use((err, request, response, next) => {
+  console.log("test");
+  if (err instanceof ApiError) {
+    return response.status(err.status).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+  return response.status(500).json({
+    message: "Internal Server Error!",
+  });
+});
+
 // Start the server and start to sisten at the port (...)
 const port = 4000;
 app.listen(port, () => {

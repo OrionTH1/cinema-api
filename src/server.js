e3484a2a66c682ef337fa94afad7ae63ff5aadf3
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2024-06-05 15:53:58 -0300

    fix: add app.listen to end of the code

--- src/server.js
+++ src/server.js
@@ -14,3 +14,9 @@ app.use(express.json());
 
 // Import Routes
 app.use(routes);
+
+// Start the server and start to sisten at the port (...)
+const port = 4000;
+app.listen(port, () => {
+  console.log("Server start at port:", port);
+});
